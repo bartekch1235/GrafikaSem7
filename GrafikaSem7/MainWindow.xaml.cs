72f8b4120356c6020d62f4d3b469da640ffcfd8c
bartekch1235 <82156135+bartekch1235@users.noreply.github.com>
2023-11-06 12:14:53 +0100

    first part done

--- GrafikaSem7/MainWindow.xaml.cs
+++ GrafikaSem7/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
 using System.IO.Pipes;
 using System.Linq;
@@ -10,6 +11,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Ink;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -18,231 +20,200 @@ using System.Windows.Shapes;
 
 namespace grafzad3
 {
+    public enum TransformationType
+    {
+        add, sub, mul, div , bri, gray
+    }
     public partial class MainWindow : Window
     {
-        enum pType { p1, p2 }
-        public event Action<string> LogEvent;
-
+        int redValue = 0;
+        int blueValue = 0;
+        int greenValue = 0;
+        int factorValue = 1;
 
         public MainWindow()
         {
-
-
             InitializeComponent();
-            OpenImage();
+            SetImage();
 
         }
 
 
-        #region  zad3
-        string GetStringFromFileDialog()
+
+        void SetImage()
         {
             string fileContent = string.Empty;
             OpenFileDialog dialog = new OpenFileDialog();
             dialog.FileName = "Document";
-            dialog.DefaultExt = ".txt";
-            //dialog.Filter = "Text documents (*.txt)|*.txt|P1 files (*.pbm)|*.pbm|P2Files (*.pgm)|*.pgm";
-            dialog.Filter = "Text documents (*.txt)|*.txt|P1 files (*.pbm)|*.pbm|P2Files (*.pgm)|*.pgm";
+            dialog.DefaultExt = ".png";
+            dialog.Filter = "zdjecie (*.png)|*.png";
 
             bool? result = dialog.ShowDialog();
-            Stream file;
 
-            file = dialog.OpenFile();
-            using (StreamReader reader = new StreamReader(file))
-            {
-                fileContent = reader.ReadToEnd();
-            }
-            return fileContent;
+            Image.Source = new BitmapImage(new Uri(dialog.FileName));
+
         }
-        void OpenImage()
+
+
+        void GenerateImage(TransformationType type)
         {
-            pType imageType = pType.p1;
+            BitmapSource bitmapSource = (BitmapSource)Image.Source;
+            WriteableBitmap writeableBitmap = new WriteableBitmap(bitmapSource);
 
-            List<byte> pixelByteArray = new List<byte>();
+            int stride = writeableBitmap.PixelWidth * 4;
+            int size = writeableBitmap.PixelHeight * stride;
+            byte[] pixels = new byte[size];
+            writeableBitmap.CopyPixels(pixels, stride, 0);
 
-            int heigh = 0, width = 0;
-
-            string fileContent = GetStringFromFileDialog();
-
-            //MessageBox.Show(fileContent, "File Content at path: ", MessageBoxButton.OK);
-
-
-            int first3varaibles = 0;//ustawiwanie metadata
-            for (int i = 0; i < fileContent.Length; i++)
+            switch (type)
             {
-                if (fileContent[i].Equals('#'))
-                    fileContent = DeleteCommentedLine(fileContent, i, out i);
+                case TransformationType.add:
+                    pixels = AddRGB(pixels);
+                    break;
 
-                if (!IsWhiteSign(fileContent[i]))
+                case TransformationType.sub:
+                    pixels = SubRGB(pixels);
+                    break;
+                case TransformationType.div:
+                    pixels = DivRGB(pixels);
+                    break;
+                case TransformationType.mul:
+                    pixels = MulRGB(pixels);
+                    break;                
+                case TransformationType.bri:
+                    pixels = ChangeBrightness(pixels);
+                    break;
+                case TransformationType.gray:
+                    pixels = ChangeToGray(pixels);
+                    break;
+
+            }
+
+
+
+            writeableBitmap.WritePixels(new Int32Rect(0, 0, writeableBitmap.PixelWidth, writeableBitmap.PixelHeight), pixels, stride, 0);
+            Image.Source = writeableBitmap;
+
+        }
+        byte[] AddRGB(byte[] pixels)
+        {
+            for (int i = 0; i < pixels.Length; i += 4)
+            {
+                pixels[i] = (byte)Math.Min(255, pixels[i] + blueValue);
+                pixels[i + 1] = (byte)Math.Min(255, pixels[i + 1] + greenValue);
+                pixels[i + 2] = (byte)Math.Min(255, pixels[i + 2] + redValue);
+            }
+            return pixels;
+        }
+        byte[] SubRGB(byte[] pixels)
+        {
+            for (int i = 0; i < pixels.Length; i += 4)
+            {
+                pixels[i] = (byte)Math.Max(0, pixels[i] - blueValue);
+                pixels[i + 1] = (byte)Math.Max(0, pixels[i + 1] - greenValue);
+                pixels[i + 2] = (byte)Math.Max(0, pixels[i + 2] - redValue);
+            }
+            return pixels;
+        }
+        byte[] MulRGB(byte[] pixels)
+        {
+            for (int i = 0; i < pixels.Length; i += 4)
+            {
+                pixels[i] = (byte)Math.Min(255, pixels[i] * blueValue);
+                pixels[i + 1] = (byte)Math.Min(255, pixels[i + 1] * greenValue);
+                pixels[i + 2] = (byte)Math.Min(255, pixels[i + 2] * redValue);
+            }
+            return pixels;
+        }
+        byte[] DivRGB(byte[] pixels)
+        {
+            if (redValue == 0|| blueValue == 0 || greenValue == 0 )
+            {
+                MessageBox.Show("Divison by 0");
+            }
+            else
+            {
+                for (int i = 0; i < pixels.Length; i += 4)
                 {
-                    var extracted = ExtractNumber(fileContent, i, out i);
-
-                    if (first3varaibles == 0)
-                    {
-                        if (extracted.ToLower().Equals("p1"))
-                            imageType = pType.p1;
-                        if (extracted.ToLower().Equals("p2"))
-                            imageType = pType.p2;
-                    }
-                    else if (first3varaibles == 1)
-                    {
-                        width = int.Parse(extracted);
-                    }
-                    else if (first3varaibles == 2)
-                    {
-                        heigh = int.Parse(extracted);
-                    }
-                    else
-                    {
-                        if (imageType == pType.p1)//musialem tak zrobic bo nie wiadomo dlaczego  nie dziala bitmapsource.Create() w przypadku 1 bitow pixeli
-                        {
-                            if (extracted.Length > 1)
-                            {
-                                char[] splitted = extracted.ToCharArray();
-                                foreach (char s in splitted)
-                                {
-
-
-                                    if (int.Parse(s.ToString()).Equals(0))
-                                        pixelByteArray.Add((byte)0);
-                                    else if (int.Parse(s.ToString()).Equals(1))
-                                        pixelByteArray.Add((byte)255);
-                                }
-                            }
-                            else
-                            {
-                                if (int.Parse(extracted).Equals(0))
-                                    pixelByteArray.Add((byte)0);
-                                else if (int.Parse(extracted).Equals(1))
-                                    pixelByteArray.Add((byte)255);
-                            }
-
-                        }
-                        else
-                        {
-                            pixelByteArray.Add((byte)int.Parse(extracted));
-                        }
-
-                    }
-                    first3varaibles++;
+                    pixels[i] = (byte)Math.Max(0, pixels[i] / blueValue);
+                    pixels[i + 1] = (byte)Math.Max(0, pixels[i + 1] / greenValue);
+                    pixels[i + 2] = (byte)Math.Max(0, pixels[i + 2] / redValue);
                 }
             }
-            GenerateImage(width, heigh, pixelByteArray, imageType);
-
+            return pixels;
         }
-
-        void GenerateImage(int width, int heigh, List<Byte> pixels, pType imageType)
+        byte[] ChangeBrightness(byte[] pixels)
         {
-            var array = pixels.ToArray();
-
-            BitmapSource bitmapSource = null;
-
-            int rawStride = (width * 8 + 7) / 8;
-            bitmapSource = BitmapSource.Create(width, heigh, 300, 300, PixelFormats.Indexed8, BitmapPalettes.Gray256, array, rawStride);
-
-            Image.Source = bitmapSource;
-
-        }
-
-        bool IsWhiteSign(char c)
-        {
-            if ((c.Equals('\n') || c.Equals(' ') || c.Equals('\t') || c.Equals('\r')))
+            for (int i = 0; i < pixels.Length; i += 4)
             {
-                return true;
+                pixels[i] = (byte)Math.Clamp( pixels[i] + factorValue,0,255);
+                pixels[i + 1] = (byte)Math.Clamp(pixels[i+1] + factorValue, 0, 255);
+                pixels[i + 2] = (byte)Math.Clamp(pixels[i+2] + factorValue, 0, 255);
             }
-            return false;
+            return pixels;
         }
-        string DeleteCommentedLine(string file, int pos, out int i)
+
+        byte[] ChangeToGray(byte[] pixels)
         {
-            char[] fileInChar = file.ToCharArray();
-            while (!file[pos].Equals('\n'))
+            for (int i = 0; i < pixels.Length; i += 4)
             {
+                var avg= (pixels[i]+ pixels[i+1]+ pixels[i+2])/ 3;
 
-                fileInChar[pos] = ' ';
-
-                pos++;
+                pixels[i] = (byte)avg;
+                pixels[i + 1] = (byte)avg;
+                pixels[i + 2] = (byte)avg;
             }
-            i = pos;
-            return new string(fileInChar);
+            return pixels;
         }
-        string ExtractNumber(string file, int pos, out int i)
+
+
+        private void Add_RGB_Click(object sender, RoutedEventArgs e)
         {
-            string exctractedNumber = string.Empty;
-            while (file.Length > pos && !IsWhiteSign(file[pos]))
-            {
-                exctractedNumber += file[pos];
-
-                pos++;
-            }
-
-            i = pos;
-            return exctractedNumber;
+            GenerateImage(TransformationType.add);
         }
-
-
-
-        void SaveImageAsP2(BitmapSource bitmapSource)
+        private void Sub_RGB_Click(object sender, RoutedEventArgs e)
         {
-            int arrayCount = bitmapSource.PixelHeight * bitmapSource.PixelWidth;
-            byte[] ar = new byte[arrayCount];
-            var stride = (bitmapSource.PixelWidth * 8 + 7) / 8;
-            bitmapSource.CopyPixels(ar, stride, 0);
-
-
-            var imageData = "p2 \n" + bitmapSource.PixelWidth + " " + bitmapSource.PixelHeight + "\n";
-            foreach (byte b in ar)
-            {
-                imageData += b.ToString() + "\n";
-            }
-
-
-            string docPath =
-              Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-
-            using (StreamWriter outputFile = new StreamWriter(System.IO.Path.Combine(docPath, "ImageP2.txt")))
-            {
-                outputFile.WriteLine(imageData);
-                MessageBox.Show("File was saved in documents folder!");
-            }
+            GenerateImage(TransformationType.sub);
         }
-        void SaveImageAsP1(BitmapSource bitmapSource)
+
+        private void RedTextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-            int arrayCount = bitmapSource.PixelHeight * bitmapSource.PixelWidth;
-            byte[] ar = new byte[arrayCount];
-            var stride = (bitmapSource.PixelWidth * 8 + 7) / 8;
-            bitmapSource.CopyPixels(ar, stride, 0);
-
-
-            var imageData = "p1 \n" + bitmapSource.PixelWidth + " " + bitmapSource.PixelHeight + "\n";
-            foreach (byte b in ar)
-            {
-                if (b == 0)
-                    imageData += b.ToString() + "\n";
-                else
-                    imageData += 1 + "\n";
-            }
-
-
-            string docPath =
-              Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-
-            using (StreamWriter outputFile = new StreamWriter(System.IO.Path.Combine(docPath, "ImageP2.txt")))
-            {
-                outputFile.WriteLine(imageData);
-                MessageBox.Show("File was saved in documents folder!");
-            }
+            int.TryParse(RedTextBox.Text, out redValue);
         }
 
-
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        private void GreenTextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-            SaveImageAsP1(Image.Source as BitmapSource);
+            int.TryParse(GreenTextBox.Text, out greenValue);
         }
-        private void Button_Click(object sender, RoutedEventArgs e)
+
+        private void BlueTextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-            SaveImageAsP2(Image.Source as BitmapSource);
+            int.TryParse(BlueTextBox.Text, out blueValue);
         }
-        #endregion
+
+        private void Multiplication_Click(object sender, RoutedEventArgs e)
+        {
+            GenerateImage(TransformationType.mul);
+        }
+
+        private void Divison_Click(object sender, RoutedEventArgs e)
+        {
+            GenerateImage(TransformationType.div);
+        }
+
+        private void FactorTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            int.TryParse(FactorTextBox.Text, out factorValue);
+        }
+
+        private void Brightness_Click(object sender, RoutedEventArgs e)
+        {
+            GenerateImage(TransformationType.bri);
+        }
+
+        private void Gray_Click(object sender, RoutedEventArgs e)
+        {
+            GenerateImage(TransformationType.gray);
+        }
     }
 }
